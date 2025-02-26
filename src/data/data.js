b222918db50ae4e83086dce8db5a8975451439ed
Ethan Anderson <150651098+mite404@users.noreply.github.com>
2025-02-25 20:27:32 -0500

    Set Up Basic Layout & Functionality
    - add menuArray
    - create event listeners for addToCart and removeFromCart
    - create functions to display menu, cart, find an item's index to delete from cart

--- src/data/data.js
+++ src/data/data.js
@@ -1,7 +1,7 @@
 export const menuArray = [
   {
     name: "Pizza",
-    ingredients: ["pepperoni", "mushrom", "mozarella"],
+    ingredients: ["pepperoni", "mushroom", "mozzarella"],
     id: 0,
     price: 14,
     emoji: "🍕"
