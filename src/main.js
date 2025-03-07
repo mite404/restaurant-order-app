e1c5aa95700c1cb8ac9267291976d55a4e085d01
Ethan Anderson <150651098+mite404@users.noreply.github.com>
2025-03-07 16:52:28 -0500

    Refactor event listeners into arrow functions & refactor modal into boolean toggle.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import menuArray from "./data/menuArray.js";
 import User from "./models/User.js";
 import { renderMenu } from "./views/view.js";
-import { handleAddItemClick, handleRemoveItemClick, cart } from "./utils/utils.js";
+import { handleAddItemClick, handleRemoveItemClick } from "./utils/utils.js";
 
 const menuContainer = document.getElementById("menu-container");
 const modal = document.getElementById("modal");
@@ -9,7 +9,7 @@ const backdrop = document.getElementById("modal-backdrop");
 const form = document.getElementById("paymentConsentForm")
 
 // Event Listeners
-document.addEventListener("click", function (e) {
+document.addEventListener("click", (e) => {
   const target = e.target;
 
   if (target.dataset.addbtn) {
@@ -23,11 +23,11 @@ document.addEventListener("click", function (e) {
   // Modal Event Listeners
   } else if (target.id === "completebtn") {
     console.log("`completebtn` EventListener attached");
-    handleCompleteBtnClick();
+    toggleModalVisibility(true);
 
   } else if (target.id === "modal-close-btn") {
     console.log("`modal-close-btn` EventListener attached");
-    handleCloseModalBtnClick();
+    toggleModalVisibility(false);
 
   // Payment Consent Form Event Listeners
   } else if (target.id === "paymentBtn") {
@@ -43,9 +43,8 @@ document.addEventListener("click", function (e) {
   }
 });
 
-
 // Credit Card input - numbers only
-document.addEventListener("input", function (e) {
+document.addEventListener("input", (e) => {
   const target = e.target;
 
   if (target.id === "card-field") {
@@ -62,21 +61,16 @@ document.addEventListener("input", function (e) {
   }
 });
 
-function handleCompleteBtnClick() {
-  const shouldShowModal = true;
-
-  if (shouldShowModal) {
-    console.log("#modal rendered!");
+function toggleModalVisibility(isVisible) {
+  if (isVisible) {
     modal.classList.remove("hidden");
     modal.classList.add("visible");
     backdrop.classList.add("visible");
+  } else {
+    modal.classList.add("hidden");
+    modal.classList.remove("visible");
+    backdrop.classList.remove("visible");
   }
-}
-
-function handleCloseModalBtnClick() {
-  modal.classList.add("hidden");
-  modal.classList.remove("visible");
-  backdrop.classList.remove("visible");
 }
 
 function addCreditCard(name, cardNumber, cvv) {
